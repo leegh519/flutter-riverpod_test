aa8f1a844a618df7cc43c7558a037e73a7fb40b1
leegh519 <105862138+leegh519@users.noreply.github.com>
2023-03-13 17:22:38 +0900

    Delete river_pod directory

--- river_pod/android/app/src/main/kotlin/com/example/river_pod/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.river_pod
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- river_pod/lib/provider/state_provider.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final stateProvider = StateProvider<int>((ref) => 0);
